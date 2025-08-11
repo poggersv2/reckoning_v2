f77bc924640e7fda51e0db8c60968e957fcc152f
poggersv2 <131923140+poggersv2@users.noreply.github.com>
2025-08-11 19:41:18 +1200

    stacked motor code

--- reckoning_v2/src/main.cpp
+++ reckoning_v2/src/main.cpp
@@ -1,13 +1,6 @@
 #include "main.h"
-// #include "pros/adi.h" 
 #include "pros/adi.hpp"
 #include "pros/misc.h"
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
 
 void on_center_button() {
 	static bool pressed = false;
@@ -19,12 +12,6 @@ void on_center_button() {
 	}
 }
 
-/**
- * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
- */
 void initialize() {
 	pros::lcd::initialize();
 	pros::lcd::set_text(1, "Hello PROS User!");
@@ -32,74 +19,37 @@ void initialize() {
 	pros::lcd::register_btn1_cb(on_center_button);
 }
 
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
 void disabled() {}
 
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
 void competition_initialize() {}
 
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
- 
+
+
 // Controller setup
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
 // Motor setup
-pros::MotorGroup left_mg({1, 2, 3}); // left motors
-pros::MotorGroup right_mg({-11, -12, -13}); // right motors
-pros::Motor bottom_intake(18); // bottom intake
-pros::Motor top_intake(-20); // top intake
+pros::MotorGroup left_mg({-11, 12, 13}); // left motors
+pros::MotorGroup right_mg({1, -2, -3}); // right motors
+pros::Motor bottom_intake(10); // bottom intake
+pros::Motor top_intake(9); // top intake
 
 // Pneumatics setup
 pros::adi::Pneumatics blocker('A', false); // blocker piston
 pros::adi::Pneumatics ramp('B', false);    // ramp piston
+
 
 void autonomous() {
 	// Drive forward at full speed for 1 second, then stop
 	left_mg.move(-50);
 	right_mg.move(-80);
 	pros::delay(800);
-	blocker.set_value(true);
-	top_intake.move(127);
+	//blocker.set_value(true);
+	//top_intake.move(127);
 	
 	left_mg.move(0);
 	right_mg.move(0);
 }
-
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
 
 void opcontrol() {
 	// State vars
