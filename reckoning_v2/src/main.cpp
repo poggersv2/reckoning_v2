56600b200dee8d4e3e7fa771831693b8a3ce79b1
poggersv2 <131923140+poggersv2@users.noreply.github.com>
2025-08-29 21:10:35 +1200

    PID + new motor ports
    BASE

--- reckoning_v2/src/main.cpp
+++ reckoning_v2/src/main.cpp
@@ -1,9 +1,9 @@
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
 #include "main.h" 
-#include "pros/motors.h"
+#include "pros/misc.h"
 #include "pros/motors.hpp"
-// #include "pros/misc.h"
-// #include "pros/optical.hpp" // for optical sensor
-// #include "pros/rtos.hpp"
+#include "pros/rtos.hpp"
 #include <cmath> // for pow() and fabs()
 
 void initialize() {}
@@ -16,89 +16,133 @@ void competition_initialize() {}
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
 // Optical sensor setup
-pros::Optical optical_sensor(14);
+// pros::Optical optical_sensor(14);
 
 // Drivetrain setup
-pros::Motor left_front(8, pros::v5::MotorGear::blue);
-pros::Motor left_middle(9, pros::v5::MotorGear::blue);
-pros::Motor left_back(-10, pros::v5::MotorGear::blue);
+// Top stacked motors should be reversed (negative number)
 
-pros::Motor right_front(-1, pros::v5::MotorGear::blue);
-pros::Motor right_middle(2, pros::v5::MotorGear::blue);
-pros::Motor right_back(-3, pros::v5::MotorGear::blue);
-
-pros::MotorGroup left_mg({8, 9, -10}, pros::v5::MotorGears::blue); // left motors
-pros::MotorGroup right_mg({-1, 2, -3}, pros::v5::MotorGears::blue); // right motors
+pros::MotorGroup left_mg({4, -5, -6}, pros::v5::MotorGears::blue); // left motor group
+pros::MotorGroup right_mg({-1, 2, 3}, pros::v5::MotorGears::blue); // right motor group
 
 // Motor Setup
-pros::Motor top_intake(5);
-pros::Motor bottom_intake(6);
+pros::Motor top_intake(7);
+pros::Motor bottom_intake(19);
+pros::Motor bottom_back_intake(-18);
 
-/* PID  *
- * WIP! 
- * Reference: https://www.vexforum.com/t/how-to-implement-a-pid/92878/2
- // */
+// PID variables
+// Constants
+double kp = 0.2;
+double ki = 0.0;
+double kd = 0.0;
 
-void drive_pid(double target, double kp, double ki, double kd) {
-    double error = 0; 
-    double prev_error = 0;
-    double integral = 0;
-    double derivative = 0;
+// Other variables
+// Straight drive
+int error; // sensor value - target value (ositional value)
+int prev_error = 0; // position 20 ms ago
+int derivative; // error - prev_error (calculates speed)
+int total_error = 0; // total_error + error 
 
-    const double tolerance = 5; // acceptable error in encoder ticks
-    const int max_time = 3000;   // timeout in ms
-    int elapsed_time = 0;
+// Turning
+double turn_error = 0;
+double turn_prev_error = 0;
+double turn_derivative = 0;
+double turn_integral = 0;
 
-    // Reset encoders
+bool pid_enabled = true;
+
+void turnAngle (int degrees, int max_time_ms) {
+    // Reset PID state
+    double turn_error = 0;
+    double turn_prev_error = 0;
+    double turn_derivative = 0;
+    double turn_integral = 0;
+    // Reset motor encoders before driving
     left_mg.tare_position();
     right_mg.tare_position();
 
-    while (elapsed_time < max_time) {
-        // Calculate average position of drivetrain
+    int start_time = pros::millis();
+    while (true) {
+        // Average drivetrain position
         double left_pos = left_mg.get_position();
         double right_pos = right_mg.get_position();
-        double average_pos = (left_pos + right_pos) / 2.0;
+        double avg_pos = (left_pos - right_pos) / 2.0; // difference in position for turning
 
         // PID calculations
-        error = target - average_pos;
-        integral += error;
+        turn_error = degrees - avg_pos;
+        turn_derivative = turn_error - turn_prev_error;
+        turn_integral += turn_error;
+
+        double turn_motor_power = (kp * turn_error) + (ki * turn_integral) + (kd * turn_derivative);
+
+        // Apply to drivetrain
+        left_mg.move(turn_motor_power);
+        right_mg.move(-turn_motor_power); // reverse power for turning
+
+        turn_prev_error = turn_error;
+
+        // Exit conditions
+        if (fabs(turn_error) < 5) break;  // Close enough to target
+        if (pros::millis() - start_time > max_time_ms) break;  // Timeout
+
+        pros::delay(20);
+    }
+
+    // Stop drivetrain
+    left_mg.move(0);
+    right_mg.move(0);
+}
+
+double mmToMotorDegrees(double distance_mm) {
+    const double wheel_diameter = 100.0; // change if different wheel
+    const double wheel_circumference = wheel_diameter * M_PI;
+    const double gear_ratio = 48.0 / 36.0; // wheel / motor
+    return (distance_mm / wheel_circumference) * 360.0 * gear_ratio;
+}
+
+void driveDistance(int target, int max_time_ms) {
+    target = mmToMotorDegrees(target);
+    // Reset PID state
+    double error = 0;
+    double prev_error = 0;
+    double derivative = 0;
+    double integral = 0;
+    // Reset motor encoders before driving
+    left_mg.tare_position();
+    right_mg.tare_position();
+
+    int start_time = pros::millis();
+    while (true) {
+        // Average drivetrain position
+        double left_pos = left_mg.get_position();
+        double right_pos = right_mg.get_position();
+        double avg_pos = (left_pos + right_pos) / 2.0;
+
+        // PID calculations
+        error = target - avg_pos;
         derivative = error - prev_error;
+        integral += error;
 
-        // PID output
-        double output = (kp * error) + (ki * integral) + (kd * derivative);
+        double motor_power = (kp * error) + (ki * integral) + (kd * derivative);
 
-        // Move drivetrain
-        left_mg.move(output);
-        right_mg.move(output);
+        // Apply to drivetrain
+        left_mg.move(motor_power);
+        right_mg.move(motor_power);
 
         prev_error = error;
 
-        // Exit if within tolerance
-        if (fabs(error) < tolerance) break;
+        // Exit conditions
+        if (fabs(error) < 10) break;  // Close enough to target
+        if (pros::millis() - start_time > max_time_ms) break;  // Timeout
 
         pros::delay(20);
-        elapsed_time += 20;
     }
-
-    // Stop motors
+    // Stop drivetrain
     left_mg.move(0);
     right_mg.move(0);
 }
 
 void autonomous() {
-	drive_pid(1000, 0.3, 0.0, 0.2);
-}
-
-double exponential_drive(double x) {
-    double sign;
-    if (x >= 0) {
-        sign = 1.0;
-    } else {
-        sign = -1.0;
-    }
-    double mag = fabs(x);               
-    double y = 1.2 * pow(1.043, mag) - 1.2 + 0.2 * mag;
-    return sign * y;                 
+    driveDistance(1000, 1000);
 }
 
 void opcontrol() {
@@ -113,22 +157,37 @@ void opcontrol() {
 		// double right_speed = exponential_drive(left_stick);
 
 		// Move motors
-		left_mg.move(exponential_drive(left_stick));
-		right_mg.move(exponential_drive(right_stick));
+		left_mg.move(left_stick);
+		right_mg.move(right_stick);
 		pros::delay(20);
 
-        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
-            top_intake.move(-127);
+        // OUTTAKE 
+        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
+            top_intake.move(127);
             bottom_intake.move(-127);
-        } else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
+            bottom_back_intake.move(-127);
+        // INTAKE
+        } else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
             top_intake.move(127);
             bottom_intake.move(127);
+            bottom_back_intake.move(127);
+        // MIDDLE OUT
+        } else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
+            top_intake.move(-127);
+            bottom_intake.move(127);
+            bottom_back_intake.move(-127);
+        // TOP
+        } else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
+            top_intake.move(127);
+            bottom_intake.move(127);
+            bottom_back_intake.move(-127);
         } else {
             top_intake.move_voltage(0);
             bottom_intake.move_voltage(0);
+            bottom_back_intake.move_voltage(0);
         };
 
-		optical_sensor.disable_gesture();
+		// .disable_gesture();
     	pros::delay(20);
 	}
 }
